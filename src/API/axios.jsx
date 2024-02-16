815b9f71eaa2dc3c0e2fd36f4ce5b0de6455732f
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-02-16 18:42:40 +0300

    completing the project

--- src/API/axios.jsx
+++ src/API/axios.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
+    //firebase baseurl
     // baseURL:"http://127.0.0.1:5001/e-clone-3a65b/us-central1/api"
+    // express baseurl
     baseURL:"https://amazon-backend-first-deploy.onrender.com"
 
 })
